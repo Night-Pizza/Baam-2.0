d17ea237225ceaf08142ddc7cf9fd92bfe9cb0d6
Katharina Gross <katharina270907@gmail.com>
2026-04-09 20:07:18 +0300

    frontend

---
+++ frontend/src/pages/Profile.tsx
@@ -0,0 +1,9 @@
+import AppShell from '../components/AppShell';
+
+export default function Profile() {
+    return (
+        <AppShell title="Profile">
+            <p>Profile</p>
+        </AppShell>
+    );
+}
